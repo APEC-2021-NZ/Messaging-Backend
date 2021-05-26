cca803cd78f1cf91970d5cc948a11e86e408b446
ZHENK <chengzhenyang@gmail.com>
2021-05-27 02:47:35 +1200

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const httpServer = createServer()
+const io = new Server(httpServer, {
+    // ...
+})
+
+io.on('connection', (socket) => {
+    const { conversations } = socket.handshake.query
+    conversations.map((conversation) => socket.join(conversation))
+
+    socket.on('message', (conversation, message) => {
+        socket.to(conversation).emit(message)
+    })
+})
+
+httpServer.listen(3000)
