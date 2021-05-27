f013e31f5f9dd68b9f1c2641a4c38b8e8c48bc28
ZHENK <chengzhenyang@gmail.com>
2021-05-27 16:47:05 +1200

    add express

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import { createServer } from 'http'
+import express from 'express'
 import { Server } from 'socket.io'
 import dotenv from 'dotenv'
 
 dotenv.config()
 
-const httpServer = createServer()
+const httpServer = createServer(express())
 const io = new Server(httpServer, {
     cors: { origin: '*', methods: ['GET', 'POST'] },
 })
@@ -18,4 +19,5 @@ io.on('connection', (socket) => {
     })
 })
 
+console.log('start server at http://localhost:3000')
 httpServer.listen(3000)
