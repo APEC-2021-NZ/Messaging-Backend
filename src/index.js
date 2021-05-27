218bccf51afe79ec480be5bce6f7dd17b6571f81
ZHENK <chengzhenyang@gmail.com>
2021-05-27 16:28:58 +1200

    add cors

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ dotenv.config()
 
 const httpServer = createServer()
 const io = new Server(httpServer, {
-    // ...
+    cors: { origin: '*', methods: ['GET', 'POST'] },
 })
 
 io.on('connection', (socket) => {
