0069365bb52db08fd022207bef796c07715245a0
ZHENK <chengzhenyang@gmail.com>
2021-05-28 08:51:29 +1200

    fix cors

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import cors from 'cors'
 dotenv.config()
 
 const app = express()
-app.use(cors)
+app.use(cors())
 
 const httpServer = createServer(app)
 const io = new Server(httpServer, {
@@ -16,15 +16,20 @@ const io = new Server(httpServer, {
 
 io.on('connection', (socket) => {
     const { conversations } = socket.handshake.query
-    conversations.map((conversation) => socket.join(conversation))
+    if (conversations !== undefined) {
+        console.log(conversations)
+        conversations.map((conversation) => socket.join(conversation))
+    }
 
     socket.on('join', ({ conversation }) => {
+        console.log('someone joined', conversation)
         socket.join(conversation)
     })
     socket.on('message', ({ conversation, message }) => {
-        socket.to(conversation).emit('message', { conversation, message })
+        console.log('someone sent', message, 'to', conversation)
+        io.to(conversation).emit('message', { conversation, message })
     })
 })
 
-console.log('start server at http://localhost:3000')
-httpServer.listen(process.env.PORT || 3000)
+console.log('start server at http://localhost:3001')
+httpServer.listen(process.env.PORT || 3001)
